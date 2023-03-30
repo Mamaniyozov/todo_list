1824f18da663c535d322c7b45e4f0cf4078c805a
Mmaniyozov <mamaniyozovmuhammadyusuf5@gmail.com>
2023-03-30 09:30:52 +0500

    delete url

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path
-from api.views import add
+from api.views import add,delete_task
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('add/',add),
+    path('delte/',delete_task),
 ]
