64fb7d8aa832791cb0833c685b85a2d61efba511
Mmaniyozov <mamaniyozovmuhammadyusuf5@gmail.com>
2023-03-30 11:45:50 +0500

    is to ready for sayt

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path
-from api.views import add,delete_task
+from api.views import add,delete_task,index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('add/',add),
     path('delete/',delete_task),
+    path('', index),
 ]
